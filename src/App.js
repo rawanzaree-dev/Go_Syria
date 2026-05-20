2e2f8b8c302eab79cacf28bf0729e7013677b666
Rawan Zaree <rawan.m.zaree@gmail.com>
2026-05-20 19:06:43 +0300

    Created Services Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import HeroHeader from './components/hero-header/HeroHeader';
+import Services from './components/services/Services';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroHeader />
+      <Services />
     </div>
   );
 }
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,26 @@
+import './services.css';
+
+export default function Services() {
+    return (
+        <div className="services">
+            <div className="container">
+                <div className="item">
+                    <i class="bi bi-card-image service-icon"></i>
+                    Adventures
+                </div>
+                <div className="item">
+                    <i class="bi bi-people-fill service-icon"></i>
+                    Family Toures
+                </div>
+                <div className="item">
+                    <i class="bi bi-buildings service-icon"></i>
+                    City Cards
+                </div>
+                <div className="item">
+                    <i class="bi bi-globe service-icon"></i>
+                    Multi-Day Trips
+                </div>
+            </div>
+        </div>
+    );
+}
