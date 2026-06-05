6e2903368d89c620c0f33043ee0a6df07b5ebdea
Rawan Zaree <rawan.m.zaree@gmail.com>
2026-06-05 13:16:47 +0300

    Created Home Page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,34 @@
+import HeroHeader from '../../components/hero-header/HeroHeader';
+import Services from '../../components/services/Services';
+import Tours from '../../components/tours/Tours';
+import Pagination from "../../components/pagination/Pagination";
+import Sort from "../../components/sort/Sort";
+import Banner from '../../components/banner/Banner';
+import NewsLetter from '../../components/news-letter/NewsLetter';
+import {toursList} from "../../data.js";
+import { useState } from 'react';
+import { pagination } from '../../utils/pagination.js';
+import { sorting } from '../../utils/sorting.js';
+
+export default function Home() {
+    const [currentPage, setCurrentPage]= useState(1);
+
+    // Sort Tours
+    const [sortItem, setSortItem] = useState("recommended");
+
+    const sortedTours= sorting(sortItem, toursList);
+
+    const {pages, pageTours} = pagination(toursList.length, sortedTours, currentPage);
+    
+    return (
+        <>
+            <HeroHeader />
+            <Services />
+            <Sort length={toursList.length} sortItem={sortItem} setSortItem={setSortItem} />
+            <Tours toursList={pageTours} />
+            <Pagination pages={pages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+            <Banner />
+            <NewsLetter />
+        </>
+    );
+}
