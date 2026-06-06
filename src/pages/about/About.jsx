bf863490a08e24618ceaddd8a9bc766d4d9f69b8
Rawan Zaree <rawan.m.zaree@gmail.com>
2026-06-06 19:25:45 +0300

    Created About Page

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,13 @@
+import "./about.css";
+
+export default function About() {
+    return (
+        <div className="about-us">
+            <h1>About This App</h1>
+            <div className="info">
+                This App is All About Booking Tours in Syria
+                <p><span>Version: </span>1.0.0</p>
+            </div>
+        </div>
+    );
+}
